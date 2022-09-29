e36d134c4d5fe58c5d644374a9884f0b4efff451
Ferrozo <61892198+Ferrozo@users.noreply.github.com>
2022-09-29 22:27:46 +0100

    themeCubit test

--- test/theme/cubit/theme_cubit_test.dart
+++ test/theme/cubit/theme_cubit_test.dart
@@ -1,13 +1,80 @@
 import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:weather_app_with_bloc/theme/cubit/theme_cubit.dart';
 import 'package:weather_repository/weather_repository.dart';
 import 'package:mocktail/mocktail.dart';
 
-import '../../helpers/hydrated_bloc.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
 
 class MockWeather extends Mock implements Weather {
   MockWeather(this._condition);
 
   final WeatherCondition _condition;
+
+  @override
+  WeatherCondition get condition => _condition;
+}
+
+void main() {
+  // initHydratedStorage();
+
+  group('ThemeCubit', () {
+    test('initial state is correct', () {
+      expect(ThemeCubit().state, ThemeCubit.defaultColor);
+    });
+
+    group('toJson/fromJson', () {
+      test('work properly', () {
+        final themeCubit = ThemeCubit();
+        expect(
+          themeCubit.fromJson(themeCubit.toJson(themeCubit.state)),
+          themeCubit.state,
+        );
+      });
+    });
+
+    group('updateTheme', () {
+      final clearWeather = MockWeather(WeatherCondition.clear);
+      final snowyWeather = MockWeather(WeatherCondition.snowy);
+      final cloudyWeather = MockWeather(WeatherCondition.cloudy);
+      final rainyWeather = MockWeather(WeatherCondition.rainy);
+      final unknownWeather = MockWeather(WeatherCondition.unknown);
+
+      blocTest<ThemeCubit, Color>(
+        'emits correct color for WeatherCondition.clear',
+        build: ThemeCubit.new,
+        act: (cubit) => cubit.updateTheme(clearWeather),
+        expect: () => <Color>[Colors.orangeAccent],
+      );
+
+      blocTest<ThemeCubit, Color>(
+        'emits correct color for WeatherCondition.snowy',
+        build: ThemeCubit.new,
+        act: (cubit) => cubit.updateTheme(snowyWeather),
+        expect: () => <Color>[Colors.lightBlueAccent],
+      );
+
+      blocTest<ThemeCubit, Color>(
+        'emits correct color for WeatherCondition.cloudy',
+        build: ThemeCubit.new,
+        act: (cubit) => cubit.updateTheme(cloudyWeather),
+        expect: () => <Color>[Colors.blueGrey],
+      );
+
+      blocTest<ThemeCubit, Color>(
+        'emits correct color for WeatherCondition.rainy',
+        build: ThemeCubit.new,
+        act: (cubit) => cubit.updateTheme(rainyWeather),
+        expect: () => <Color>[Colors.indigoAccent],
+      );
+
+      blocTest<ThemeCubit, Color>(
+        'emits correct color for WeatherCondition.unknown',
+        build: ThemeCubit.new,
+        act: (cubit) => cubit.updateTheme(unknownWeather),
+        expect: () => <Color>[ThemeCubit.defaultColor],
+      );
+    });
+  });
 }
